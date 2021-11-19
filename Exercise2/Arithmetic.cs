965c87ca502652165c7377f836e9c8565d0d098d
WRay <83263910+wzr0008@users.noreply.github.com>
2021-11-18 22:41:13 -0600

    HW2 EXERCIES2

---
+++ Exercise2/Arithmetic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise2
+{
+    public class Arithmetic
+    {
+        public void Addition(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine($"The sum between {a} and {b} is {sum}.");
+            return;
+        }
+
+        public void Subtraction(int a, int b)
+        {
+            int sub = a - b;
+            Console.WriteLine($"The substraction between {a} and {b } is {sub}.");
+        }
+
+        public void Multiplication(int a, int b)
+        {
+            int mult = a * b;
+            Console.WriteLine($"The multiplication between {a} and {b} is {mult}");
+        }
+
+        public void Division(int a, int b)
+        {
+            int div = a / b;
+            Console.WriteLine($"The division between {a} and {b} is {div}");
+        }
+    }
+}
